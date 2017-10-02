bc78727507402bb238327b37a027d3011cd53fc8
Dominik Wojtowicz <dominik.s.wojtowicz@gmail.com>
2017-10-02 18:51:52 +0200

    projects checkpoint

--- js/script.js
+++ js/script.js
@@ -1,6 +1,128 @@
 $(document).ready(function () {
+
     $(document).scroll(function () {
         var $nav = $(".navbar-fixed-top");
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
+
+
+    // ------------ jQuery SVG 3D Tag Cloud Plugin --------------- //
+    var entries = [
+
+        {
+            label: 'HTML5',
+            url: 'https://en.wikipedia.org/wiki/HTML5',
+            target: '_top'
+        },
+        {
+            label: 'CSS3',
+            url: 'https://en.wikipedia.org/wiki/Cascading_Style_Sheets',
+            target: '_top'
+        },
+        {
+            label: 'Bootstrap',
+            url: 'http://getbootstrap.com/',
+            target: '_top'
+        },
+        {
+            label: 'SASS',
+            url: 'http://sass-lang.com/',
+            target: '_top'
+        },
+        {
+            label: 'JavaScript',
+            url: 'https://en.wikipedia.org/wiki/JavaScript',
+            target: '_top'
+        },
+        {
+            label: 'JQuery',
+            url: 'https://jquery.com/',
+            target: '_top'
+        },
+        {
+            label: 'Angular 4',
+            url: 'https://angular.io/',
+            target: '_top'
+        },
+        {
+            label: 'WordPress',
+            url: 'https://pl.wordpress.org/',
+            target: '_top'
+        },
+        {
+            label: 'WooCommerce',
+            url: 'https://woocommerce.com/',
+            target: '_top'
+        },
+        {
+            label: 'PHP',
+            url: 'https://en.wikipedia.org/wiki/PHP',
+            target: '_top'
+        },
+        {
+            label: 'RWD',
+            url: 'https://en.wikipedia.org/wiki/Responsive_web_design',
+            target: '_top'
+        },
+        {
+            label: 'GitHub',
+            url: 'https://github.com/',
+            target: '_top'
+        },
+        {
+            label: 'Git',
+            url: 'https://en.wikipedia.org/wiki/Git',
+            target: '_top'
+        },
+        {
+            label: 'Gimp',
+            url: 'https://www.gimp.org/',
+            target: '_top'
+        }        
+
+    ];
+
+    var settings = {
+
+        entries: entries,
+        width: 640,
+        height: 480,
+        radius: '65%',
+        radiusMin: 75,
+        bgDraw: true,
+        bgColor: 'rgba(1, 1, 1, 0)',
+        opacityOver: 1.00,
+        opacityOut: 0.05,
+        opacitySpeed: 6,
+        fov: 800,
+        speed: 1.5,
+        fontFamily: 'Oswald, Arial, sans-serif',
+        fontSize: '15',
+        fontColor: '#fff',
+        fontWeight: 'normal', //bold
+        fontStyle: 'normal', //italic 
+        fontStretch: 'normal', //wider, narrower, ultra-condensed, extra-condensed, condensed, semi-condensed, semi-expanded, expanded, extra-expanded, ultra-expanded
+        fontToUpperCase: true
+
+    };
+
+    //var svg3DTagCloud = new SVG3DTagCloud( document.getElementById( 'holder'  ), settings );
+    $('#tag-cloud').svg3DTagCloud(settings);
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-36251023-1']);
+    _gaq.push(['_setDomainName', 'jqueryscript.net']);
+    _gaq.push(['_trackPageview']);
+
+    (function () {
+        var ga = document.createElement('script');
+        ga.type = 'text/javascript';
+        ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(ga, s);
+    })();
+    // ------------ jQuery SVG 3D Tag Cloud Plugin --------------- //
+
+
 });
